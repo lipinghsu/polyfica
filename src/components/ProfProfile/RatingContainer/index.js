d80ee317a8303fa7c8c96f9c7729f83aeb285c4e
LIPING <liping@Li-Ping.local>
2024-07-24 08:15:55 -0700

    adjust ProfProfile responsive layout

--- src/components/ProfProfile/RatingContainer/index.js
+++ src/components/ProfProfile/RatingContainer/index.js
@@ -2,13 +2,26 @@ import React from 'react';
 
 const RatingContainer = ({ qualityRating, difficultyRating }) => (
     <div className="ratingContainer">
-        <div className="ratingLabel">Quality</div>
-        <div className="qualityRatingRow ratingRow">
-            <div className="ratingValue">{parseFloat(qualityRating).toFixed(1)}</div>
+        <div className='rating-wrap'>
+            <div className="ratingLabel">
+                Quality
+            </div>
+            <div className="qualityRatingRow ratingRow">
+                <div className="ratingValue">
+                    {parseFloat(qualityRating).toFixed(1)}
+                </div>
+            </div>
         </div>
-        <div className="ratingLabel">Difficulty</div>
-        <div className="difficultyRatingRow ratingRow">
-            <div className="ratingValue">{parseFloat(difficultyRating).toFixed(1)}</div>
+
+        <div className='rating-wrap'>
+            <div className="ratingLabel">
+                Difficulty
+            </div>
+            <div className="difficultyRatingRow ratingRow">
+                <div className="ratingValue">
+                    {parseFloat(difficultyRating).toFixed(1)}
+                </div>
+            </div>
         </div>
     </div>
 );
